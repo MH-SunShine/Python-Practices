f37feb022ef662d594db5e23d8a733be0a5c4be5
Marwa Hammamouche <123541378+MH-SunShine@users.noreply.github.com>
2024-07-30 12:46:27 +0100

    about docstring concept

---
+++ docstring_concept.py
@@ -0,0 +1,10 @@
+# practicing the doc string concept
+
+def call_my_name():
+    """ this function does one single gorgeous thing
+    it calls my beautiful name"""
+
+    return print("hello Marwa")
+
+
+print(call_my_name.__doc__)
